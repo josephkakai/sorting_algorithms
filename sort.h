be9094feb101af4729ca5ddba2d486c04060206c
Joseph Kakai <josephngalu96@gmail.com>
2023-01-28 12:57:19 -0800

    Cocktail sort

--- sort.h
+++ sort.h
@@ -37,6 +37,9 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
+/* swapping functions*/
+void swap_nodes(listint_t **list, listint_t *node);
+
 /* Sorting algoritms */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
